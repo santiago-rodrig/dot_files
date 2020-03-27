f164718f8e754705490cd7e54af80af21c18c220
Santiago Rodríguez <santo1996.29@gmail.com>
2020-03-27 17:24:09 -0400

    first stage of install script

--- install.sh
+++ install.sh
@@ -1,2 +1,50 @@
-mv ./tmux.conf $HOME/.tmux.conf
-mv ./vimrc $HOME/.vimrc
+VIMFILE="$HOME/.vimrc"
+TMUXFILE="$HOME/.tmux.conf"
+LIGHT_GREEN='\e[92m'
+LIGHT_CYAN='\e[96m'
+RESET_COLORS='\e[0m'
+INVERTED_COLORS='\e[7m'
+RESET_INVERTED_COLORS='\e[27m'
+
+alias echo='echo -e'
+
+echo "${LIGHT_CYAN}MOVE: $INVERTED_COLORS"
+echo "\ttmux.conf -> $HOME/.tmux.conf"
+echo "\tvimrc -> $HOME/.vimrc$RESET_INVERTED_COLORS$RESET_COLORS"
+mv vimrc $VIMFILE
+mv tmux.conf $TMUXFILE
+
+# this function clones the repo user and project supplied in $1
+
+function cloneRepo () {
+  git clone https://github.com/$1 $2
+}
+
+# this function expects the intended path where to place the plugin
+# $1 is something in the form ./colors/start
+
+function checkDirs () {
+  if [ ! -d $1 ]; then
+    mkdir -p $1
+    echo "${LIGHT_GREEN}CREATE DIRS: $INVERTED_COLORS"
+    echo "\t$1$RESET_INVERTED_COLORS$RESET_COLORS"
+  fi
+}
+
+# check for the existence of $HOME/.vim/pack
+
+checkDirs "$HOME/.vim/pack"
+
+# change directory to $HOME/.vim/pack
+
+echo "${LIGHT_CYAN}CHANGE DIRECTORY: $INVERTED_COLORS"
+colors
+editing
+git
+html
+markdown
+navigation
+ruby
+status-bar
+textobjects
+tmux
--- vimrc
+++ vimrc
@@ -5,13 +5,43 @@ set noswapfile
 set relativenumber
 set encoding=UTF-8
 set number
+set foldmethod=indent
+set foldlevel=100 " Arbitrarily big number
 set colorcolumn=81
 set shiftwidth=2
 set tabstop=2
 set softtabstop=2
 set expandtab
 set nowrap
+set hlsearch
+set cursorline
+set cursorcolumn
+set noshowmode
+map <C-h> :set nohlsearch<CR>
 set pastetoggle=<F3>
 let mapleader=','
+colorscheme gruvbox
+set bg=dark
 nnoremap <leader>d :tabnext<CR>
 nnoremap <leader>s :tabprevious<CR>
+" CtrlP settings
+let g:ctrlp_map = '<c-p>'
+let g:ctrlp_cmd = 'CtrlP'
+let g:ctrlp_working_path_mode = 'ra'
+" NERDTree
+nmap <C-n> :NERDTreeToggle<CR>
+let NERDTreeShowHidden=1
+" NERDTree git flags support
+let g:NERDTreeShowIgnoredStatus=1
+" Tmuxline
+let g:airline#extensions#tmuxline#enabled=0
+" Git gutter
+set updatetime=100
+" Airline
+let g:airline#extensions#tabline#enabled=1
+let g:airline_powerline_fonts=1
+" Airline theme
+let g:airline_theme='gruvbox'
+" Markdown Preview
+nmap <C-m> :MarkdownPreviewToggle<CR>
+let g:mkdp_echo_preview_url=1
