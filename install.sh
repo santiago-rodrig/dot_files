4a9a94aaeb0d57af1f90ce4f852c383393c7606e
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-13 18:06:33 -0400

    only vim files and plugins

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 # declare main variables
 
 VIMFILE="$HOME/.vimrc"
-TMUXFILE="$HOME/.tmux.conf"
-TMUXLINEFILE="$HOME/.tmuxline.conf"
 LIGHT_GREEN="\e[92m"
 LIGHT_CYAN="\e[96m"
 RESET_COLORS="\e[0m"
@@ -14,37 +12,33 @@ RESET_INVERTED_COLORS="\e[27m"
 # define function to reset the colors after coloring output
 
 function resetColors () {
-  echo -e -n -e "$RESET_INVERTED_COLORS$RESET_COLORS"
-  echo -e -n -e "$RESET_INVERTED_COLORS$RESET_COLORS"
+  echo -en "$RESET_INVERTED_COLORS$RESET_COLORS"
+  echo -en "$RESET_INVERTED_COLORS$RESET_COLORS"
 }
 
 # define function to color the output of an informational message
 
 function beginInfo () {
-  echo -e -n -e "$LIGHT_CYAN$1"
+  echo -en "$LIGHT_CYAN$1"
   resetColors
-  echo -e -e ": $INVERTED_COLORS"
+  echo -e ": $INVERTED_COLORS"
 }
 
 # define function to color the output of a creation message
 
 function beginCreate () {
-  echo -e -n -e "$LIGHT_GREEN$1"
+  echo -en "$LIGHT_GREEN$1"
   resetColors
-  echo -e -e "$INVERTED_COLORS"
+  echo -e "$INVERTED_COLORS"
 }
 
 
 # move the configuration files to their corresponding place
 
 beginInfo 'COPY'
-echo -e -e "\ttmux.conf -> $HOME/.tmux.conf"
-echo -e -e "\tvimrc -> $HOME/.vimrc"
-echo -e -e "\ttmuxline.conf -> $HOME/.tmuxline.conf"
+echo -e "\tvimrc -> $HOME/.vimrc"
 resetColors
 cp vimrc $VIMFILE
-cp tmux.conf $TMUXFILE
-cp tmuxline.conf $TMUXLINEFILE
 
 # this function expects the intended path where to place the plugin
 # $1 is something in the form ./colors/start
@@ -52,7 +46,7 @@ cp tmuxline.conf $TMUXLINEFILE
 function checkDir () {
   if [ ! -d $1 ]; then
     beginCreate 'CREATE'
-    echo -e -e "\t$1"
+    echo -e "\t$1"
     resetColors
     mkdir -p $1
   fi
@@ -92,7 +86,7 @@ checkDir $HOME/.vim/pack
 # check for the existence of each plugin category
 
 function parsePlugin () {
-  TAIL=`echo -e -n $1 | cut -d '/' -f 2`
+  TAIL=`echo -en $1 | cut -d '/' -f 2`
 
   checkRepo $2$TAIL $1
 }
@@ -100,7 +94,7 @@ function parsePlugin () {
 # loop through the categories and get the plugins for each in turn
 
 for CATEGORY in 'colors' 'editing' 'git' 'html' 'markdown' 'navigation' \
-  'ruby' 'status-bar' 'textobjects' 'tmux'; do
+  'ruby' 'status-bar' 'textobjects'; do
   FULL_DIR="$HOME/.vim/pack/$CATEGORY/start/"
 
   checkDir $FULL_DIR
@@ -203,17 +197,6 @@ for CATEGORY in 'colors' 'editing' 'git' 'html' 'markdown' 'navigation' \
       resetColors
 
       for PLUGIN in 'kana/vim-textobj-user' 'kana/vim-textobj-entire'; do
-        beginInfo 'INFO'
-        echo -e "\tparsing $PLUGIN"
-        resetColors
-        parsePlugin $PLUGIN $FULL_DIR
-      done;;
-    'tmux' )
-      beginInfo 'INFO'
-      echo -e "\tgetting $CATEGORY category plugins"
-      resetColors
-
-      for PLUGIN in 'edkolev/tmuxline.vim'; do
         beginInfo 'INFO'
         echo -e "\tparsing $PLUGIN"
         resetColors
