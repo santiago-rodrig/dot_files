85b3fe937439f6070c07d143f070d4f6937b06e2
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-14 14:21:25 -0400

    create assets folder, prepare for the copy

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+    "os"
+    "fmt"
+    "io/ioutil"
+)
+
+var homeDir string
+var vimConfigFile os.File
+
 func main() {
+    // sets the user home directory
+    homeDir = os.GetEnv("HOME")
+    // gets the contents of the vim configuration file
+    vimConfigFile, err = ioutil.ReadFile("./assets/vimrc")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
 }
