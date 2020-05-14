dc2626de122a4785833b9df59b80bafd19b49459
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-14 18:12:44 -0400

    main refactored
    main.go now uses the download package created for fetching the contents from the internet

--- download/download.go
+++ download/download.go
@@ -1,36 +1,71 @@
 package download
 
 import (
+    "strings"
     "os"
     "net/http"
     "io"
     "log"
 )
 
-func handleError(err error) {
+// handles errors
+func handleError(err error) bool {
     if err != nil {
         log.Fatal("Error:", err)
+        return false
     }
+
+    return true
 }
 
+// prepares $HOME/.vim/autoload
 func prepareVimFolder() (err error) {
     fileMode := os.FileMode(os.ModeDir)
     home, err := os.UserHomeDir()
-    handleError(err)
+    if !handleError(err) { return }
     err = os.MkdirAll(home + "/.vim/autoload", fileMode)
-    handleError(err)
+    if !handleError(err) { return }
     return
 }
 
-func download(url string) (err error) {
+// downloads from the given url
+func download(url string) (body io.ReadCloser, err error) {
+    response, err := http.Get(url)
+    if !handleError(err) { return }
+    body = response.Body
+    return
 }
 
 // Gets the contents of the vimrc file under assets
 // and writes them to $HOME/.vimrc
-func FetchVimrc() error {
+func FetchVimrc() (err error) {
+    body, err := download(strings.Join([]string{
+        "https://raw.githubusercontent.com/santiago-rodrig",
+        "vim-setup/master/assets/vimrc",
+    }, "/")
+    if !handleError(err) { return }
+    home, err := os.UserHomeDir()
+    if !handleError(err) { return }
+    vimrc, err := os.Create(home + "/.vimrc")
+    if !handleError(err) { return }
+    _, err = io.Copy(vimrc, body)
+    return
 }
 
 // Gets the contents of plug.vim and writes them to
 // $HOME/.vim/autoload/plug.vim
-func FetchVimPlug() error {
+func FetchVimPlug() (err error) {
+    body, err := download(strings.Join([]string{
+        "https://raw.githubusercontent.com/junegunn",
+        "vim-plug/master/plug.vim",
+    }, "/")
+    if !handleError(err) { return }
+    home, err := os.UserHomeDir()
+    if !handleError(err) { return }
+    err = prepareVimFolder()
+    if !handleError(err) { return }
+    plug, err := os.Create(home + "/.vim/autoload/plug.vim")
+    if !handleError(err) { return }
+    io.Copy(plug, body)
+    return
 }
--- main.go
+++ main.go
@@ -1,87 +1,39 @@
 package main
 
 import (
+    "github.com/santiago-rodrig/vim-setup/download"
     "os"
     "os/exec"
-    "io"
     "log"
-    "strings"
-    "time"
-    "net/http"
 )
 
-func main() {
-    // sets the user home directory
-    homeDir := os.Getenv("HOME")
-    // gets the contents of the vim configuration file
-    vimConfigURL := strings.Join([]string{
-        "https://raw.githubusercontent.com/santiago-rodrig",
-        "vim-setup/master/assets/vimrc",
-    },"/")
-    response, err := http.Get(vimConfigURL)
+func installVimPlugins() (err error) {
+    home, err := os.UserHomeDir()
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Error: can't access the user home directory")
         return
     }
-    // writes the contents to the vim configuration file
-    userVimFile, err := os.Create(homeDir + "/.vimrc")
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
-    defer userVimFile.Close()
-    _, err = io.Copy(userVimFile, response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    time.Sleep(time.Second)
-    log.Println("$HOME/.vimrc has been set up")
-    // look for the existence of vim on the system
-    _, err = exec.LookPath("vim")
-    if err != nil {
-        log.Println("Please install vim: https://www.vim.org/download.php")
-        log.Fatal(err)
-        return
-    }
-    // look for the existence of git on the system
-    _, err = exec.LookPath("git")
-    if err != nil {
-        log.Println("Please install git: https://git-scm.com/downloads")
-        log.Fatal(err)
-        return
-    }
-    // set the command for installing vim-plug
-    vimPlugURL := strings.Join([]string{
-        "https://raw.githubusercontent.com/junegunn",
-        "vim-plug/master/plug.vim",
-    },"/")
-    cmd := exec.Command(
-        "curl",
-        "-fLo",
-        homeDir + "/.vim/autoload/plug.vim",
-        "--create-dirs",
-        vimPlugURL,
-    )
-    err = cmd.Run()
-    if err != nil {
-        log.Fatal("Fetching vim plug did not work")
-        return
-    }
-    // install vim plugins
     cmd = exec.Command(
         "vim",
         "-E",
         "-s",
         "-u",
-        homeDir + "/.vimrc",
+        home + "/.vimrc",
         "+PlugInstall",
         "+qa",
     )
     err = cmd.Run()
     if err != nil {
-        log.Fatal("Installing plugins with vim plug did not work")
+        log.Fatal("Error: Installing plugins with vim plug did not work")
         return
     }
-    time.Sleep(time.Second)
     log.Println("Vim plugins successfully installed")
+}
+
+func main() {
+    // make the preparations
+    download.FetchVimPlug()
+    download.FetchVimrc()
+    // install vim plugins
+    installVimPlugins()
 }
