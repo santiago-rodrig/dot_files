c07355575364646f0b5a7afdb7ab12d14511cb89
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-14 15:02:06 -0400

    add working dir instead of relative paths

--- main.go
+++ main.go
@@ -11,19 +11,25 @@ import (
 func main() {
     // sets the user home directory
     homeDir := os.Getenv("HOME")
+    workingDir, err := os.Getwd()
+    if err != nil {
+	    fmt.Println(err)
+    }
     // gets the contents of the vim configuration file
-    vimConfigFile, err := ioutil.ReadFile("./assets/vimrc")
+    vimConfigFile, err := ioutil.ReadFile(
+	    strings.Join([]string{workingDir,"/assets/vimrc"},"/"),
+    )
     if err != nil {
         fmt.Println(err)
         return
     }
     // writes the contents to the vim configuration file
-    writeErr := ioutil.WriteFile(
+    err := ioutil.WriteFile(
         strings.Join([]string{homeDir,".vimrc"},"/"),
         vimConfigFile,
         os.FileMode(0777),
     )
-    if writeErr != nil {
+    if err != nil {
         fmt.Println(writeErr)
         return
     }
