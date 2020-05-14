340bedfeca6dd6ee52560b1859fd20721989bab1
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-14 14:30:59 -0400

    delete existing vimrc if there is any

--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
     "os"
     "fmt"
     "io/ioutil"
+    "strings"
 )
-
-var homeDir string
-var vimConfigFile os.File
 
 func main() {
     // sets the user home directory
-    homeDir = os.GetEnv("HOME")
+    homeDir := os.GetEnv("HOME")
     // gets the contents of the vim configuration file
-    vimConfigFile, err = ioutil.ReadFile("./assets/vimrc")
+    vimConfigFile, err := ioutil.ReadFile("./assets/vimrc")
     if err != nil {
         fmt.Println(err)
         return
+    }
+    // deletes existing user vimrc if there is any
+    err := os.Remove(strings.Join([]string{homeDir,".vimrc"},"/"))
+    if err != nil {
+        fmt.Println(err)
     }
 }
